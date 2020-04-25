8982c73b8d52b6980b4bdebd886906ab144f6d1a
sosibona <empty.tara@gmail.com>
2020-04-25 18:02:46 +0300

    Updates

---
+++ precache-manifest.1d6d9ecb51adffbe57aca02249a28849.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350b923decf78782154685c7d89cf253",
+    "url": "/react-clicker/index.html"
+  },
+  {
+    "revision": "512731f26cdc5d56eeb4",
+    "url": "/react-clicker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3a69287bcdb11a9fd33",
+    "url": "/react-clicker/static/js/2.d9511537.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clicker/static/js/2.d9511537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512731f26cdc5d56eeb4",
+    "url": "/react-clicker/static/js/main.e13921de.chunk.js"
+  },
+  {
+    "revision": "8cc3d117579c932bc0d3",
+    "url": "/react-clicker/static/js/runtime-main.03e79a96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clicker/static/media/logo.5d5d9eef.svg"
+  }
+]);
